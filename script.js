673b748b36b010a9938bde994653e79dda45bdd8
Merika <merikatoby143@gmail.com>
2023-05-31 08:27:39 -0700

    Rules of the quiz html, css styling, js of rules

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. You will have only <span>15 seconds</span> per each question. 
+// 2. Once you select your answer, you can't reselect. 
+// 3. You can't select any option once time goes off. 
+// 4. You can't exit from the Quiz while you're playing. 
+// 5. You'll get points on the basis of your correct answers. 
+
+// W
