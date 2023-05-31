4125e447723f0a786944697763c88daa44f7a3e5
Merika <merikatoby143@gmail.com>
2023-05-31 09:53:15 -0700

    more questions + some styling

--- script.js
+++ script.js
@@ -4,4 +4,47 @@
 // 4. You can't exit from the Quiz while you're playing. 
 // 5. You'll get points on the basis of your correct answers. 
 
-// W
+// Which of the following fuction of String object returns the calling string value converted to upper case while respecting the current locale? 
+// A -toLocaleUpperCase()
+// B -toUpperCase()
+// C -toString()
+// D -substring()
+// Answer:A Explanation: toLocaleUpperCase()-Returns the calling string calue converted to uppercase while respecting the current locale. 
+
+// Which of the following fuction of Array object calls a function for each element in the array?
+// A -concat()
+// B -every()
+// C -filter()
+// D -forEach()
+// Answer:D Explanation: forEach() -Calls a function for each element in the array 
+
+// Inside which element do you put Javascript?
+// A - <var>
+// B - <script>
+// C - <section>
+// D - <code>
+// Answer: B 
+
+// Which of the following is not a primitive data type in JavaScript?
+// A - Number
+// B - String
+// C- Boolean
+// D - Object
+// Answer: D Object
+// Explanation: In JavaScript, the primitive data types are Number, String, Boolean, Null, and Undefined. Object is not a primitive data type.
+
+// What does the “typeof” operator do in JavaScript?
+// A - Returns the data type of a variable
+// B - Checks if a variable is defined
+// C - Assigns a value to a variable
+// D - Concatenates two strings
+// Answer: A- Returns the data type of a variable
+// Explanation: The “typeof” operator in JavaScript returns the data type of a variable. For example, “typeof 42” would return “number”.
+
+// Which of the following is not a comparison operator in JavaScript?
+// A - ==
+// B - ===
+// c - !=
+// D - =<
+// Answer: D - =<
+// Explanation: The correct comparison operator is “<=”, not “=<“.
